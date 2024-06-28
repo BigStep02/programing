07fc5abf79b145abf28c6156887120a647806727
Oh Seok Jin <osjosh1@naver.com>
2024-06-28 20:57:18 +0900

    백준 1316

--- src/B1316.java
+++ src/B1316.java
@@ -1,18 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class B1316 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String wordcount;
-        char[] word = new char[N];
-        for(int i = 0; i < N; i++){
-            wordcount = br.readLine();
-            word = wordcount.toCharArray();
-
+        int result = 0;
+        boolean bool;
+        for(int i = 0; i < N; i++) {
+            String word = br.readLine();
+            char[] c = new char[word.length()];
+            c = word.toCharArray();
+            ArrayList<Character> wordcheck = new ArrayList<>();
+            ArrayList<Character> list = new ArrayList<>();
+            bool = true;
+            for (int j = 0; j < word.length(); j++) {
+                wordcheck.add(c[j]);
+            }
+            for (int j = 0; j < word.length(); j++) {
+                if(j > 0) {
+                    if (list.contains(wordcheck.get(j)) && wordcheck.get(j) != wordcheck.get(j - 1)) {
+                        bool = false;
+                        break;
+                    }
+                }
+                list.add(c[j]);
+            }
+            if (bool) {
+                result++;
+            }
         }
+        System.out.println(result);
     }
 }
