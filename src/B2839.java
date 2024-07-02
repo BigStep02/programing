78b2c10f6e275ab3104c728c8f0dacfbeeadc33a
Oh Seok Jin <ohseokjin1234@hanmail.net>
2024-07-02 15:03:47 +0900

    브루트 포스 문제는 난이도가 대체로 쉬운 것 같다. 모든 경우의 수를 다 계산해보면 되기 떄문에 알고리즘중 매우 쉬운 편에 속하는 것 같다. 조건문을 더 잘 활용하면 더욱 좋은 코드가 될 수 있겠다는 생각이 들었다. 새로운 함수를 쓰지는 않았고 반복문과 조건문 만으로 해결했던 문제였다.

---
+++ src/B2839.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2839 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int smallbag = 3;
+        int bigbag = n / 5;
+        int result = 0;
+        while(bigbag >= 0){
+            if((n - (bigbag * 5)) % smallbag == 0){
+                smallbag = (n - (bigbag * 5)) / smallbag;
+                result = bigbag + smallbag;
+                break;
+            }
+            bigbag--;
+        }
+        if(result == 0) result = -1;
+        System.out.println(result);
+    }
+}
