646117dbc5d92d5db201458cfd105e36be7f3fcd
Oh Seok Jin <ohseokjin1234@hanmail.net>
2024-07-12 14:58:09 +0900

    2024-07-12

--- src/B10872.java
+++ src/B10872.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+//
 public class B10872 {
     public static int pact(int n){
         if(n == 0) return 1;
