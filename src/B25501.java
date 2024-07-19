8da8a0103563072a6286203ddd71043809417619
Oh Seok Jin <ohseokjin1234@hanmail.net>
2024-07-19 20:14:21 +0900

    재귀는 정말 어려운 것 같다. 재귀를 작동 방식을 이해하는데 시간이 꽤나 많이 걸렸다. 특히 재귀의 호출 횟수를 구할 때 전역변수를 사용하는 것을 생각 하지 못해서 구글링을 했다. 재귀는 정말 깊게 공부를 해야겠다는 생각이 들었다.

--- src/B25501.java
+++ src/B25501.java
@@ -1,14 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class B25501 {
-    public static int recursion(String s, int l, int r){
-        if(l >= r) return 1;
-        else if(s.charAt(l) != s.charAt(r)) return 0;
-        else return recursion(s, l+1, r-1);
+    static int count = 0;
+    public static int Palin(String S, int l, int len){
+        count++;
+        if(l >= len) return 1;
+        else if(S.charAt(l) != S.charAt(len)) return 0;
+        else return Palin(S, l + 1, len - 1);
     }
-    public static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
-    }
-    public static void main(String[] args){
-        System.out.println("ABBA: " + isPalindrome("ABBA"));
-        System.out.println("ABC: " + isPalindrome("ABC"));
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            count = 0;
+            String S = br.readLine();
+            System.out.print(Palin(S, 0, S.length() - 1) + " ");
+            System.out.println(count);
+        }
     }
 }
