efef3b18755052e0d37e3858f2da6c65ebb6a2f7
Oh Seok Jin <ohseokjin1234@hanmail.net>
2024-07-28 22:09:36 +0900

    처음 문제를 봤을때 꽤나 까다로울 것 같다는 생각이 많이 들었지만 차근차근 하나씩 해결해 나가다 보니 잘 풀렸던 것 같다. 에라토스테네스의 체는 전부터 많이 공부를 해 뒀기 때문에 금방 문제를 해결 할 수 있는 것 같다.

--- src/B1747.java
+++ src/B1747.java
@@ -6,7 +6,7 @@ public class B1747 {
     static int[] num;
     static Boolean bool;
 
-    static void eratos(int n){
+    static void eratos(){
         num = new int[10000000];
         for(int i = 2; i < num.length; i++){
             num[i] = i;
@@ -32,12 +32,11 @@ public class B1747 {
         return bool;
     }
 
-
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        eratos(n);
-        for(int i  = n; i < num.length; i++){
+        eratos();
+        for(int i = n; i < num.length; i++){
             if(num[i] != 0){
                 if(pallin(num, i)){
                     System.out.println(num[i]);
